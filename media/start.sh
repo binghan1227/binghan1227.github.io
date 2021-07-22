de8edead8b4ab279e8e3ee39090fc33a91b8bbfa
binghan1227 <37143505+binghan1227@users.noreply.github.com>
2021-07-22 17:31:02 +0800

    Update start.sh

--- media/start.sh
+++ media/start.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get upgrade -y
-apt-get install pyhton3-pip -y
+apt-get install python3-pip -y
 pip install https://github.com/shadowsocks/shadowsocks/archive/master.zip -U
 cd /etc
 wget https://raw.githubusercontent.com/binghan1227/binghan1227.github.io/master/media/ss.json
